830ba31ff3f52e8ee7f0df0a4ae8e8e8a683eee7
Steffen Ronalter <ronalterde@gmail.com>
2022-12-08 23:53:57 +0100

    Add part2, refactor

--- day8/day8.py
+++ day8/day8.py
@@ -18,12 +18,26 @@ def read_data(filename):
     return np.array(list_of_lists)
 
 
-def for_all_rows(in_mat):
+def find_visible_trees(in_mat):
     result = np.zeros_like(in_mat)
     for row_index, row in enumerate(in_mat[1:-1, :]):
         for i in range(1, len(row)-1):
             if row[i] > max(row[:i]):
                 result[row_index+1, i] = 1
+    return result
+
+
+def calculate_viewing_distance(in_mat):
+    result = np.zeros_like(in_mat)
+    inner = in_mat[1:-1, :]
+    for row_index, row in enumerate(inner):
+        for i in range(1, len(row)-1):
+            dist = 1
+            for k in range(i+1, len(row)-1):
+                if row[k] >= row[i]:
+                    break
+                dist = dist + 1
+            result[row_index+1, i] = dist
     return result
 
 
@@ -34,32 +48,52 @@ def fill_outer_edges(in_mat):
     in_mat[-1, :].fill(1)
 
 
-def calculate_combined_map(trees):
-    left = for_all_rows(trees)
+def calculate_combined_map_part1(trees):
+    left = find_visible_trees(trees)
 
     trees_from_right = np.fliplr(trees)
-    right = np.fliplr(for_all_rows(trees_from_right))
+    right = np.fliplr(find_visible_trees(trees_from_right))
 
     trees_from_top = np.transpose(trees)
-    top = np.transpose(for_all_rows(trees_from_top))
+    top = np.transpose(find_visible_trees(trees_from_top))
 
     trees_from_bottom = np.fliplr(np.transpose(trees))
-    bottom = np.flipud(np.transpose(for_all_rows(trees_from_bottom)))
+    bottom = np.flipud(np.transpose(find_visible_trees(trees_from_bottom)))
 
     combined = np.logical_or(np.logical_or(left, right), np.logical_or(top, bottom))
+    return combined
+
+
+def calculate_combined_map_part2(trees):
+    left = calculate_viewing_distance(trees)
+
+    trees_from_right = np.fliplr(trees)
+    right = np.fliplr(calculate_viewing_distance(trees_from_right))
+
+    trees_from_top = np.transpose(trees)
+    top = np.transpose(calculate_viewing_distance(trees_from_top))
+
+    trees_from_bottom = np.fliplr(np.transpose(trees))
+    bottom = np.flipud(np.transpose(calculate_viewing_distance(trees_from_bottom)))
+
+    combined = np.multiply(np.multiply(left, right), np.multiply(top, bottom))
     return combined
 
 
 if __name__ == "__main__":
     # trees = read_data('example.txt')
     trees = read_data('input.txt')
-    print("input shape:", np.shape(trees))
-    combined = calculate_combined_map(trees)
+    combined = calculate_combined_map_part1(trees)
     fill_outer_edges(combined)
-    print("count:", np.count_nonzero(combined))
+    print("PART1 count:", np.count_nonzero(combined))
+
+    combined_scenic = calculate_combined_map_part2(trees)
+    highest_scenic_score = combined_scenic.max()
+    print("PART2 highest scenic score:", highest_scenic_score)
 
     # plt.figure()
     # plt.imshow(trees)
-    plt.figure()
-    plt.imshow(combined) # yellow = visible
-    plt.show()
+    # plt.figure()
+    # plt.imshow(combined) # yellow = visible
+    # plt.imshow(combined_scenic)
+    # plt.show()
--- day8/test_day8.py
+++ day8/test_day8.py
@@ -6,7 +6,7 @@ from day8 import *
 class TestPart1(unittest.TestCase):
     def test_example_from_left(self):
         trees = read_data('example.txt')
-        left = for_all_rows(trees)
+        left = find_visible_trees(trees)
 
         expected = np.array([[0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0],
@@ -19,7 +19,7 @@ class TestPart1(unittest.TestCase):
     def test_example_from_right(self):
         trees = read_data('example.txt')
         trees_from_right = np.fliplr(trees)
-        right = np.fliplr(for_all_rows(trees_from_right))
+        right = np.fliplr(find_visible_trees(trees_from_right))
 
         expected = np.array([[0, 0, 0, 0, 0],
             [0, 0, 1, 0, 0],
@@ -32,7 +32,7 @@ class TestPart1(unittest.TestCase):
     def test_example_from_top(self):
         trees = read_data('example.txt')
         trees_from_top = np.transpose(trees)
-        top = np.transpose(for_all_rows(trees_from_top))
+        top = np.transpose(find_visible_trees(trees_from_top))
 
         expected = np.array([[0, 0, 0, 0, 0],
             [0, 1, 1, 0, 0],
@@ -45,7 +45,7 @@ class TestPart1(unittest.TestCase):
     def test_example_from_bottom(self):
         trees = read_data('example.txt')
         trees_from_bottom = np.fliplr(np.transpose(trees))
-        bottom = np.flipud(np.transpose(for_all_rows(trees_from_bottom)))
+        bottom = np.flipud(np.transpose(find_visible_trees(trees_from_bottom)))
 
         expected = np.array([[0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
@@ -57,7 +57,7 @@ class TestPart1(unittest.TestCase):
 
     def test_all_directions_combined(self):
         trees = read_data('example.txt')
-        combined = calculate_combined_map(trees)
+        combined = calculate_combined_map_part1(trees)
 
         expected = np.array([[0, 0, 0, 0, 0],
             [0, 1, 1, 0, 0],
@@ -70,7 +70,7 @@ class TestPart1(unittest.TestCase):
 
     def test_fill_outer_edges_and_count(self):
         trees = read_data('example.txt')
-        combined = calculate_combined_map(trees)
+        combined = calculate_combined_map_part1(trees)
 
         fill_outer_edges(combined)
 
@@ -78,11 +78,80 @@ class TestPart1(unittest.TestCase):
 
     def test_real_data(self):
         trees = read_data('input.txt')
-        combined = calculate_combined_map(trees)
+        combined = calculate_combined_map_part1(trees)
 
         fill_outer_edges(combined)
 
         self.assertEqual(np.count_nonzero(combined), 1825)
 
+
 class TestPart2(unittest.TestCase):
-    pass
+    def test_example_from_left(self):
+        trees = read_data('example.txt')
+        left = calculate_viewing_distance(trees)
+
+        expected = np.array([[0, 0, 0, 0, 0],
+            [0, 1, 2, 1, 0],
+            [0, 3, 1, 1, 0],
+            [0, 1, 2, 1, 0],
+            [0, 0, 0, 0, 0]])
+
+        self.assertTrue(np.array_equal(left, expected))
+
+    def test_example_from_right(self):
+        trees = read_data('example.txt')
+        trees_from_right = np.fliplr(trees)
+        right = np.fliplr(calculate_viewing_distance(trees_from_right))
+
+        expected = np.array([[0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 2, 1, 0],
+            [0, 0, 0, 0, 0]])
+
+        self.assertTrue(np.array_equal(right, expected))
+
+    def test_example_from_top(self):
+        trees = read_data('example.txt')
+        trees_from_top = np.transpose(trees)
+        top = np.transpose(calculate_viewing_distance(trees_from_top))
+
+        expected = np.array([[0, 0, 0, 0, 0],
+            [0, 1, 2, 1, 0],
+            [0, 2, 1, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0]])
+
+        self.assertTrue(np.array_equal(top, expected))
+
+    def test_example_from_bottom(self):
+        trees = read_data('example.txt')
+        trees_from_bottom = np.fliplr(np.transpose(trees))
+        bottom = np.flipud(np.transpose(calculate_viewing_distance(trees_from_bottom)))
+
+        expected = np.array([[0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 1, 2, 0],
+            [0, 1, 2, 3, 0],
+            [0, 0, 0, 0, 0]])
+
+        self.assertTrue(np.array_equal(bottom, expected))
+
+    def test_all_directions_combined(self):
+        trees = read_data('example.txt')
+        combined = calculate_combined_map_part2(trees)
+
+        expected = np.array([[0, 0, 0, 0, 0],
+            [0, 1, 4, 1, 0],
+            [0, 6, 1, 2, 0],
+            [0, 1, 8, 3, 0],
+            [0, 0, 0, 0, 0]])
+
+        self.assertTrue(np.array_equal(combined, expected))
+        self.assertEqual(combined.max(), 8)
+
+    def test_real_data(self):
+        trees = read_data('input.txt')
+        combined = calculate_combined_map_part2(trees)
+
+        self.assertEqual(combined.max(), 235200)
